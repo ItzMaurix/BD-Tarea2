e88216c9b4ddc54fcb2b995b1ae4235dbae1c070
abner-vidal3 <abner.vidal@usm.cl>
2024-06-18 17:27:39 -0400

    Initial commit (via bun create)

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
 import { Elysia } from "elysia";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function obtener_usuario(msg){
-  const usuario = await prisma.usuario.findMany({
-    where: {
-      direccion_correo: msg,
-    },
-  });
-  if (usuario.length == 0){
-    return 0
-  }
-  return 1
-}
-const app = new Elysia().get("/existe-usuario/:msg", ({ params : { msg } }) => obtener_usuario(msg)).listen(3000);
-/*
-const newUser = await prisma.usuario.create({
-  data: {
-    nombre: 'Juanito',
-    direccion_correo: 'juanito668@juan.com',
-    clave: '1234',
-    descripcion: 'me gusta el amongus potion'
-  },
-});*/
-//console.log('Usuario creado:', newUser);
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
